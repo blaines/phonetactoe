6b673f14f3d4ed9e9ee5d3600e9b45dbf934f046
blaines <github@technologyaz.com>
2010-12-06 17:12:49 -0800

    commit

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -74,11 +74,15 @@ class GamesController < ApplicationController
     game = player.game
     # game = @game = Game.find(params[:id])
     verb = Twilio::Verb.new { |v|
-        v.say params["Digits"]
-        v.gather(:action => "/games/#{game.id}/gather.xml", :method => 'POST', :timeout => "90", :numDigits => 1) {
-          v.say 'Pick a position'
-        }
-        v.say "We didn't receive any input. Goodbye!"
+        v.say params["Digits"] if params["Digits"]
+        v.pause :length => 5
+        v.redirect "/games/#{game.id}/gather.xml"
+        if player.id.to_s == game.player_one && game.turn=true || player.id.to_s == game.player_two && game.turn=false
+          v.gather(:action => "/games/#{game.id}/gather.xml", :method => 'POST', :timeout => "90", :numDigits => 1) {
+            v.say 'Pick a position'
+          }
+          v.say "We didn't receive any input. Goodbye!"
+        end
     }
     
     
