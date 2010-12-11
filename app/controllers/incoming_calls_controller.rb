0de99648da7ccd717ef0c53cb432a8d2066e054d
blaines <github@technologyaz.com>
2010-12-10 20:32:56 -0800

    fixed sms

--- app/controllers/incoming_calls_controller.rb
+++ app/controllers/incoming_calls_controller.rb
@@ -146,7 +146,8 @@ class IncomingCallsController < ApplicationController
       if player.game.active && player.game.available
         player.game.destroy # Unstage
         player.game = nil
-        Twilio::Sms.message("(815) 216-5378", "+#{player.phone_number}", 'Get a friend to join in!', 'http://twilio-tic-tac-toe.heroku.com/incoming_texts')
+        Twilio.connect('AC1afaeecf73a8e05e32c695eac213226c', '2f4d4a952c4d0bdfa9b9d40266b6b81d')
+        Twilio::Sms.message("(815) 216-5378", "+#{player.phone_number}", 'Get a friend to join in!')
         #  Want a call back when the game is ready? (Yes or No)
       elsif player.game.active && !player.game.available
         player.hungup = true # jerk! :)
