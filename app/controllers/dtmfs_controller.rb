c6cb0320b8c73903add34e05c7d3af442642ba6a
blaines <github@technologyaz.com>
2010-12-13 16:22:03 -0700

    Added cache control so new gsm file is sent to twilio

--- app/controllers/dtmfs_controller.rb
+++ app/controllers/dtmfs_controller.rb
@@ -1,6 +1,7 @@
 class DtmfsController < ApplicationController
   def show
     respond_to do |format|
+      headers["Cache-Control"] = "no-cache"
       format.gsm { send_file "public/dtmf/#{params[:id]}.gsm", :type => "audio/x-gsm" }
     end
   end
