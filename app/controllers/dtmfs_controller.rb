33a6f62146e8dea13608fbe99fc264b3581c7454
blaines <github@technologyaz.com>
2010-12-13 17:10:58 -0700

    Added 0-9*# dtmf

--- app/controllers/dtmfs_controller.rb
+++ app/controllers/dtmfs_controller.rb
@@ -2,7 +2,7 @@ class DtmfsController < ApplicationController
   def show
     respond_to do |format|
       headers["Cache-Control"] = "no-cache"
-      format.gsm { send_file "public/dtmf/#{params[:id]}.gsm", :type => "audio/x-gsm" }
+      format.gsm { send_file "public/dtmf/#{params[:id]}.gsm" }
     end
   end
 end
